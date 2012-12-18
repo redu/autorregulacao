5d5329b43fd17b7aa2eca6ccc1ffd812f3c2d6fe
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-12-18 10:26:41 -0300

    Raw administrator layouts (without bootstrap customizations)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require modernizr
+//= require jquery.autosize.min
+//= require placeholder-polyfill.min
+//= require bootstrap-redu
+//= require holder
