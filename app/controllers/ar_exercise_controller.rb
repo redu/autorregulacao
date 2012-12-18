e5b8ad36752c466a0c276136db65ccab37baad77
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-12-18 09:02:08 -0300

    initial commit

---
+++ app/controllers/ar_exercise_controller.rb
@@ -0,0 +1,10 @@
+class ArExerciseController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autoregulation::Application.config.secret_token = 'ea340bc09e23c7538cc071dd61529c3229664f944f196ed36d1718f53f34060d26a07b34075fd7f7bb63cd6630c871fc44a240eaf97a08236c9c7fa6150713e4'
