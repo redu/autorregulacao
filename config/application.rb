daefa22d954e854e8d60dd170a258f1da6f45985
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-12-28 14:16:03 -0300

    Answer question

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Autoregulation
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/app/services)
     config.autoload_paths += %W(#{config.root}/app/forms)
+    config.autoload_paths += %W(#{config.root}/app/helpers)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
