2aa66e48543741d02101036a604f54ec54bb18a2
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-01-20 07:19:20 -0300

    Checks if the user from the session matches with the given by canvas provider

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -5,9 +5,22 @@ class BaseController < ActionController::Base
 
   protected
 
-  def current_user
+  # Gets possible loged in user from session
+  def session_user
+    return @session_user if defined? @session_user
     if session.has_key? :user_id
-      @current_user ||= User.find_by_id(session[:user_id])
+      @session_user ||= User.find_by_id(session[:user_id])
+    end
+  end
+
+  # Verifies if the loged in user is the same passed by the canvas provider
+  def current_user
+    return @current_user if defined? @current_user
+    return session_user unless params[:user_id]
+
+    user_id = params[:user_id]
+    if session_user.try(:uid) == user_id
+      @current_user ||= session_user
     end
   end
 
