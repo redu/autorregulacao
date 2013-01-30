51e2abb4a307a53024acd9affc165d0cd4440277
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-01-30 07:12:41 -0300

    Adds smtp configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,4 +71,13 @@ Autoregulation::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.host = ENV['HOST']
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'autorregulacao.herokuapp.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
